c9fdb7662c18e276a886650acf23895b9849ec6d
root <root@web.lan>
2022-02-22 00:01:28 -0600

    Agregar contenido

--- index.php
+++ index.php
@@ -2,6 +2,14 @@
 echo "<html>";
 echo "<body>";
 echo "<h1>Mario Parra Huerta</h1>";
+<br>
+echo "<h3>Licenciatura en Informatica</h3>";
+echo "<h3>8 semestre</h3>";
+<br>
+<br>
+echo "<p>Gustos: Hamburguesas, pasar tiempo con mis amigos, futbolito, ver videos en youtube y los perros</p>";
+<br>
+echo "<p>Academico: Me gustaria aprender web y tal vez testing</p>";
 echo "</body>";
 echo "</html>";
 
